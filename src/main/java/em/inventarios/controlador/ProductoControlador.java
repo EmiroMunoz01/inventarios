a5e7ed596dcb5959f062640fc5065591283d1ccd
EmiroMunoz01 <emiro_2001@icloud.com>
2024-11-12 23:15:03 -0500

    Ya tenemos el backend finalizado, de esta forma se ha creado el ultimo metodo que es la eliminacion de un producto

--- src/main/java/em/inventarios/controlador/ProductoControlador.java
+++ src/main/java/em/inventarios/controlador/ProductoControlador.java
@@ -1,10 +1,13 @@
 package em.inventarios.controlador;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,6 +63,11 @@ public class ProductoControlador {
 
         // lo buscamos nuevamente para garantizar que el producto exista en la bd
         Producto producto = this.productoServicio.buscarProductoPorId(id);
+
+        if (producto == null) {
+            throw new RecursoNoEncontradoExcepcion("No se encontro el id " + id);
+        }
+
         producto.setDescripcion(productoRecibido.getDescripcion());
         producto.setPrecio(productoRecibido.getPrecio());
         producto.setExistencia(productoRecibido.getExistencia());
@@ -68,4 +76,17 @@ public class ProductoControlador {
 
     }
 
+    @DeleteMapping("/productos/{id}")
+    public ResponseEntity<Map<String, Boolean>> eliminarProducto(@PathVariable int id) {
+        Producto producto = this.productoServicio.buscarProductoPorId(id);
+        if (producto == null) {
+            throw new RecursoNoEncontradoExcepcion("No se encontro el id " + id);
+        }
+
+        this.productoServicio.eliminarProductoPorId(producto.getIdProducto());
+        Map<String, Boolean> respuesta = new HashMap<>();
+        respuesta.put("Elimimado",
+                Boolean.TRUE);
+        return ResponseEntity.ok(respuesta);
+    }
 }
